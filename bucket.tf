f2bc4155f863a49dfac4d283080cc1ddd589c2ca
Mohammed Yahaya <daudaauwal@yahoo.co.uk>
2025-07-13 02:39:20 -0400

    first commit

--- bucket.tf
+++ bucket.tf
@@ -5,7 +5,9 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket-created-by-terraform-dynamic-secret"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "My bucket from GitHub Actions"
+    CreatedBy   = "GitHub Actions"
+    Project     = "Terraform"
     Environment = "Dev"
   }
 }
